b70b0e3720d332e685cf2068d13b0948aaea13f3
mauriciogc <mauriciogc@gmail.com>
2024-09-19 10:35:59 -0600

    added example with useNavigate

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Main = () => <h1>I'm Main page</h1>;
+const Main = () => {
+  const navigate = useNavigate();
+
+  const handleAbout = () => navigate('/about'); //add programmatic routing
+  const handleBack = () => navigate(-1); //Back programmatic routing
+  const handleForward = () => navigate(1); //Forward programmatic routing
+
+  return (
+    <div>
+      <h1>Hello, I'm Main component </h1>
+      <hr />
+      <h2>useNavigate</h2>
+      <button onClick={handleAbout}>Go to "About"</button>
+      <button onClick={handleBack}>Back</button>
+      <button onClick={handleForward}>Forward</button>
+      <hr />
+    </div>
+  );
+};
 
 export default Main;
